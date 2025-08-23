0b87a2a5726288f784748c7fce7a0e86d833f221
Aida Carabot <aidacarabot@MacBook-Pro-de-Aida-2.local>
2025-08-23 13:58:53 -0400

    fixed

--- dist/server.js
+++ dist/server.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const taskRoutes_1 = __importDefault(require("./src/api/routes/taskRoutes"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use('/tasks', taskRoutes_1.default);
